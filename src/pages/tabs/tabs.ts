e4bd5043fad8ea9cb537cd4d7b6eb6f5e829830c
= <--replace-all>
2018-03-02 13:46:58 -0600

    finish tutorial

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,25 +1,19 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the TabsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
+ 
 @IonicPage()
 @Component({
   selector: 'page-tabs',
   templateUrl: 'tabs.html',
 })
 export class TabsPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+ 
+  tab1Root: any = 'Tab1Page';
+  tab2Root: any = 'Tab2Page';
+  myIndex: number;
+ 
+  constructor(navParams: NavParams) {
+    // Set the active tab based on the passed index from menu.ts
+    this.myIndex = navParams.data.tabIndex || 0;
   }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad TabsPage');
-  }
-
 }
