c3ad955c33363caf76317afc7384deb97a8236f3
fabian <stahlschiff@web.de>
2018-03-04 10:00:12 -0600

    add awesome-fonts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 @Component({
-  templateUrl: 'app.html'
+  templateUrl: 'app.html',
 })
 export class MyApp {
   rootPage:any = 'LoginPage';
