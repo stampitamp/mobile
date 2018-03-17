b85199eee95bcd6096f3285ebda46b38cdbefa25
fabian <stahlschiff@web.de>
2018-03-17 14:17:50 +0100

    implement update of card with scrolling to top

--- src/pages/tab1/tab1.ts
+++ src/pages/tab1/tab1.ts
@@ -1,20 +1,24 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild, NgZone } from '@angular/core';
+import { IonicPage, NavController, NavParams, Content } from 'ionic-angular';
 import { Angular2TokenService } from 'angular2-token';
-
 
 @IonicPage()
 @Component({
   selector: 'page-tab1',
   templateUrl: 'tab1.html',
 })
+
 export class Tab1Page {
+
 
   cards: any;
 
+  @ViewChild(Content) content: Content;
+
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
-              private tokenService: Angular2TokenService ) {
+              private tokenService: Angular2TokenService,
+              private ngZone: NgZone ) {
   }
 
   ngOnInit() {
@@ -31,6 +35,27 @@ export class Tab1Page {
         console.log(error)
       }
     )
+  }
+
+  ngAfterViewInit() {
+    this.content.ionScroll.subscribe((data)=> {
+      if (this.content.getContentDimensions().scrollTop < 0) {
+        this.tokenService.get('home.json').subscribe(
+
+          (res) => {
+            console.log('result');
+            console.log(res.json());
+            this.ngZone.run(() => {
+              this.cards = res.json();
+            });
+
+          },
+          (error) => {
+            console.log(error)
+          }
+        )
+      }
+    });
   }
 
   ionViewDidLoad() {
