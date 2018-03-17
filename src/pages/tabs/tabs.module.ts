835bc7e03302408ca1c46f2fdf54b15a4258d0af
fabian <stahlschiff@web.de>
2018-03-17 18:45:44 +0100

    disable tab2

--- src/pages/tabs/tabs.module.ts
+++ src/pages/tabs/tabs.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { TabsPage } from './tabs';
+import { TabHiddenDirective } from './tabs.directive'
 
 @NgModule({
   declarations: [
     TabsPage,
+    TabHiddenDirective,
   ],
   imports: [
     IonicPageModule.forChild(TabsPage),
